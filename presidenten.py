3ba7c4ba13aebd338ecc2e7973cc9f060660e160
dawidjs <d.j.szefler@kjemi.uio.no>
2021-09-07 23:00:27 +0200

    Created main project file.

---
+++ presidenten.py
@@ -0,0 +1,14 @@
+import random
+
+class Deck:
+    def __init__(self):
+        """Oppretter en vanlig bunke med 52 kort"""
+        self.cards = []
+        for s in ('Ruter', 'Hjerter', 'Spar', 'Kløver'):
+            for v in range(1, 14):
+                selv.cards.append((s,v))
+
+class Game:
+    def hand_out(self):
+        return
+
