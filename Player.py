2d3a5e8d617b058118b2b2eb8868b60e7cd35f00
Dawid Szefler <dawidszefler@Air-Dawid.home>
2022-01-04 23:44:54 +0100

    self.place atributten ble fjernet og lagt inn som en liste i Game.py

--- Player.py
+++ Player.py
@@ -3,4 +3,3 @@ class Player():
         self.name = name
         self.index = index
         self.deck = deck
-        self.place = 0  # 2: President, 1: Vice President, 0: Nøytral, -1: Vice Boms, -2: Boms
