e59bb1526ba7b8b29051c9e441829395c4185fc0
Dawid Szefler <dawidszefler@Air-Dawid.home>
2022-01-05 00:11:04 +0100

    dev_mode ble flyttet til denne filen

--- presidenten.py
+++ presidenten.py
@@ -1,9 +1,14 @@
 import os
 import time
+import sys
 from Game import Game
 clear = lambda: os.system('clear')
 clear()
 
+developer_mode = False
+if len(sys.argv) != 1:
+    if sys.argv[1] == "dev":
+        developer_mode = True
 
 def main():
     game = Game()
